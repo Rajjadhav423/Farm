ff4e432c9b080d3197fb909068826f24163c6e39
Rajjadhav423 <2021bit057@sggs.ac.in>
2024-09-08 02:42:12 +0530

    auth

---
+++ server/controllers/authController.js
@@ -0,0 +1,33 @@
+const { User, validate } = require("../models/user");
+const bcrypt = require("bcrypt");
+
+// User login
+exports.loginUser = async (req, res) => {
+    try {
+        const { error } = validateLogin(req.body);
+        if (error)
+            return res.status(400).send({ message: error.details[0].message });
+
+        const user = await User.findOne({ email: req.body.email });
+        if (!user)
+            return res.status(401).send({ message: "Invalid Email or Password" });
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword)
+            return res.status(401).send({ message: "Invalid Email or Password" });
+
+        const token = user.generateAuthToken();
+        res.status(200).send({ data: token, message: "Logged in successfully" });
+    } catch (error) {
+        res.status(500).send({ message: "Internal Server Error" });
+    }
+};
+
+// Validate login data
+const validateLogin = (data) => {
+    const schema = Joi.object({
+        email: Joi.string().email().required().label("Email"),
+        password: Joi.string().required().label("Password"),
+    });
+    return schema.validate(data);
+};
